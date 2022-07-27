dee4e207841a3014836aff83df5cccd8fb3d6559
wawazoe <ngycxzbsnbf508j08gpb@docomo.ne.jp>
2022-07-27 17:51:26 +0900

    application_controller.rb　記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   private
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |nickname, password|
+      username == 'admin' && password == '2222'
+    end
   end
 end
