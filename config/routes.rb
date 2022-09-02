eb61b6681b15191d270daf5f48a5432bb16b723f
wawazoe <ngycxzbsnbf508j08gpb@docomo.ne.jp>
2022-09-02 16:47:42 +0900

    エラー解決

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: "gives#index"
   resources :gives, only:[:new, :edit, :create]
   post 'gives/new', to: 'gives#new'
+  get 'gives/edit', to: 'gives#edit'
   resources :gets, only:[:new, :edit, :create]
   post 'gets/new', to: 'gets#new'
+  post 'gets/edit', to: 'gets#edit'
 end
